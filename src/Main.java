acc17af24c317ac3b245667bac49dbb025f6bd79
Lucas <lucas.marino@pucpr.edu.br>
2024-03-27 20:38:38 -0300

    Tarefa 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Usuario usuario1 = new Usuario("João", 25, "joao@example.com", "joao123", "senha123");
+        Usuario usuario2 = new Usuario("Maria", 30, "maria@example.com", "maria456", "senha456");
+        Usuario usuario3 = new Usuario("Pedro", 35, "pedro@example.com", "pedro789", "senha789");
+    }
+}
---
+++ src/Menu.java
@@ -0,0 +1,6 @@
+public class Menu {
+    public void imprimirMenuCadastroUsuarios() {
+        System.out.println("Menu de Cadastro de Usuários");
+        System.out.println("1. Cadastrar novo usuário");
+    }
+}
